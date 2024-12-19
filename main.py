159dee9e033763db8bb12eec174841699b78b04a
Your Name <you@example.com>
2024-12-19 20:03:00 +0200

    toteutettu pelialue ja muokkaus hirviöihin https://github.com/users/12etmama/projects/3/views/1?pane=issue&itemId=91531869

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import pygame
 import random
+import sys
 
 # Pygame init
 pygame.init()
 
 # Asetukset
-# width, height = 1024, 800
 info = pygame.display.Info()
 width, height = info.current_w, info.current_h
 win = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
-# win = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Ritari ja Sotilaat")
+pygame.display.set_caption("Peli")
 
 # Värit
 white = (255, 255, 255)
@@ -24,51 +23,74 @@ player_speed = 5
 
 # Hirviön asetukset
 monster_size = 50
-monster_pos = [random.randint(0, width-monster_size), random.randint(0, height-monster_size)]
 monster_speed = 1
 
-clock = pygame.time.Clock()
+def create_monsters(num_monsters):
+    monsters = []
+    for _ in range(num_monsters):
+        monster_pos = [random.randint(0, width-monster_size), random.randint(0, height-monster_size)]
+        monsters.append(monster_pos)
+    return monsters
 
-# Pääsilmukka
-game_over = False
-while not game_over:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_over = True
+def move_monsters(monsters):
+    for monster_pos in monsters:
+        if monster_pos[0] < player_pos[0]:
+            monster_pos[0] += monster_speed
+        if monster_pos[0] > player_pos[0]:
+            monster_pos[0] -= monster_speed
+        if monster_pos[1] < player_pos[1]:
+            monster_pos[1] += monster_speed
+        if monster_pos[1] > player_pos[1]:
+            monster_pos[1] -= monster_speed
 
-    keys = pygame.key.get_pressed()
+def check_collision(player_pos, monsters):
+    for monster_pos in monsters:
+        if (abs(player_pos[0] - monster_pos[0]) < player_size and
+            abs(player_pos[1] - monster_pos[1]) < player_size):
+            return True
+    return False
 
-    if keys[pygame.K_LEFT] and player_pos[0] > 0:
-        player_pos[0] -= player_speed
-    if keys[pygame.K_RIGHT] and player_pos[0] < width - player_size:
-        player_pos[0] += player_speed
-    if keys[pygame.K_UP] and player_pos[1] > 0:
-        player_pos[1] -= player_speed
-    if keys[pygame.K_DOWN] and player_pos[1] < height - player_size:
-        player_pos[1] += player_speed
-
-    # Hirviön liike
-    if monster_pos[0] < player_pos[0]:
-        monster_pos[0] += monster_speed
-    if monster_pos[0] > player_pos[0]:
-        monster_pos[0] -= monster_speed
-    if monster_pos[1] < player_pos[1]:
-        monster_pos[1] += monster_speed
-    if monster_pos[1] > player_pos[1]:
-        monster_pos[1] -= monster_speed
-
-    # Törmäyksen tarkistus
-    if (abs(player_pos[0] - monster_pos[0]) < player_size and
-        abs(player_pos[1] - monster_pos[1]) < player_size):
-        game_over = True
-
+def draw_entities(player_pos, monsters):
     win.fill(white)
-
     pygame.draw.rect(win, black, (player_pos[0], player_pos[1], player_size, player_size))
-    pygame.draw.rect(win, red, (monster_pos[0], monster_pos[1], monster_size, monster_size))
-
+    for monster_pos in monsters:
+        pygame.draw.rect(win, red, (monster_pos[0], monster_pos[1], monster_size, monster_size))
     pygame.display.update()
 
-    clock.tick(30)
+def main(num_monsters):
+    monsters = create_monsters(num_monsters)
+    clock = pygame.time.Clock()
+    game_over = False
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
 
-pygame.quit()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_ESCAPE]:
+            game_over = True
+        if keys[pygame.K_LEFT] and player_pos[0] > 0:
+            player_pos[0] -= player_speed
+        if keys[pygame.K_RIGHT] and player_pos[0] < width - player_size:
+            player_pos[0] += player_speed
+        if keys[pygame.K_UP] and player_pos[1] > 0:
+            player_pos[1] -= player_speed
+        if keys[pygame.K_DOWN] and player_pos[1] < height - player_size:
+            player_pos[1] += player_speed
+
+        move_monsters(monsters)
+        if check_collision(player_pos, monsters):
+            game_over = True
+
+        draw_entities(player_pos, monsters)
+        clock.tick(30)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Käyttö: python peli.py <hirviöiden määrä>")
+        sys.exit(1)
+
+    num_monsters = int(sys.argv[1])
+    main(num_monsters)
